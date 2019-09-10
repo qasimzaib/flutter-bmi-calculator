42d02695040a05cc772c972c668eca8e36076b55
qasimzaib <zaib_qasim@hotmail.com>
2019-09-09 18:28:10 -0700

    Creating reusable widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,43 @@ class _InputPageState extends State<InputPage> {
 			appBar: AppBar(
 				title: Text('BMI CALCULATOR'),
 			),
-			body: Center(
-				child: Text('Body Text'),
+			body: Column(
+				children: <Widget>[
+					Expanded(
+						child: Row(
+							children: <Widget>[
+								Expanded(child: Card(color: Color(0xFF1D1E33))),
+								Expanded(child: Card(color: Color(0xFF1D1E33))),
+							],
+						),
+					),
+					Expanded(child: Card(color: Color(0xFF1D1E33))),
+					Expanded(
+						child: Row(
+							children: <Widget>[
+								Expanded(child: Card(color: Color(0xFF1D1E33),)),
+								Expanded(child: Card(color: Color(0xFF1D1E33))),
+							],
+						),
+					),
+				],
+			)
+		);
+	}
+}
+
+class Card extends StatelessWidget {
+	final Color color;
+
+	Card({@required this.color});
+
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			margin: EdgeInsets.all(15.0),
+			decoration: BoxDecoration(
+				color: color,
+				borderRadius: BorderRadius.circular(5.0),
 			),
 		);
 	}
